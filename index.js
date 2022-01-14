145b5b94d2ce1e6e6a55c284966a3260057c50b6
Chun Yi <8.5by11@gmail.com>
2022-01-14 15:54:28 -0500

    commit tree 61c8ed2168ca85595603a06eae676442d22b8660

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+// function superbowlWin(record) {
+//     const findWinningYear = record.find( record => record.result === 'W');
+//     return findWinningYear ? findWinningYear.year : undefined;
+// };
+
+
+
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+  ]
+  
+  function superbowlWin(record) {
+      const findWinningYear = record.find( record => record.result === 'W');
+  
+      if (findWinningYear) { 
+          return (findWinningYear.year);
+      } else {
+          return (undefined);
+      }
+  }
+  
+  superbowlWin(record);
